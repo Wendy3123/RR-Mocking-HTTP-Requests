2cc968e494ecd737f12157ef9189572f23405c4f
Wendy Wang <wendywangg928@gmail.com>
2023-08-24 16:04:20 -0400

    addd describe jest method to group all tests under one

--- src/App.test.js
+++ src/App.test.js
@@ -8,21 +8,24 @@ beforeEach(() => {
   fetch.resetMocks();
 });
 
-test("mock the GET request using the Jest Fetch Mock npm package", async () => {
-  fetch.mockResponseOnce(JSON.stringify({ name: "coder" }));
-  render(<App />);
-  const githubName = await waitFor(() => screen.getByRole("heading"));
-  expect(githubName).toHaveTextContent("coder");
-});
+//describe is a jest method that contains one or more related tests
+describe("receives value from github REST API using jest fetch mock", () => {
+  test("mock the GET request using the Jest Fetch Mock npm package", async () => {
+    fetch.mockResponseOnce(JSON.stringify({ name: "coder" }));
+    render(<App />);
+    const githubName = await waitFor(() => screen.getByRole("heading"));
+    expect(githubName).toHaveTextContent("coder");
+  });
 
-test("receive github URL", async () => {
-  fetch.mockResponseOnce(
-    JSON.stringify({ html_url: "https://github.com/learningToCode1234" })
-  );
-  render(<App />);
-  const githubUser = await waitFor(() => screen.getByRole("link"));
-  expect(githubUser).toHaveAttribute(
-    "href",
-    expect.stringContaining("github.com")
-  );
+  test("receive github URL", async () => {
+    fetch.mockResponseOnce(
+      JSON.stringify({ html_url: "https://github.com/learningToCode1234" })
+    );
+    render(<App />);
+    const githubUser = await waitFor(() => screen.getByRole("link"));
+    expect(githubUser).toHaveAttribute(
+      "href",
+      expect.stringContaining("github.com")
+    );
+  });
 });
