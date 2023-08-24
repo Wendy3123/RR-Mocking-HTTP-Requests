80e88a28b6b6d67edb510ab534ca8583cb4802d2
Wendy Wang <wendywangg928@gmail.com>
2023-08-24 15:48:19 -0400

    updated

--- src/App.test.js
+++ src/App.test.js
@@ -1,3 +1,28 @@
+import { waitFor } from "@testing-library/react";
+import { render } from "@testing-library/react";
+import { screen } from "@testing-library/react";
+import App from "./App";
 
+beforeEach(() => {
+  // sets everything back to initial state after each test
+  fetch.resetMocks();
+});
 
+test("mock the GET request using the Jest Fetch Mock npm package", async () => {
+  fetch.mockResponseOnce(JSON.stringify({ name: "coder" }));
+  render(<App />);
+  const githubName = await waitFor(() => screen.getByRole("heading"));
+  expect(githubName).toHaveTextContent("coder");
+});
 
+test("receive github URL", async () => {
+  fetch.mockResponseOnce(
+    JSON.stringify({ html_url: "https://github.com/learningToCode1234" })
+  );
+  render(<App />);
+  const githubUser = await waitFor(() => screen.getByRole("link"));
+  expect(githubUser).toHaveAttribute(
+    "href",
+    expect.stringContaining("github.com")
+  );
+});
